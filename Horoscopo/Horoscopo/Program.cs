acaa0c1a5e46d5bedd37214793272c8fb49efdde
Esvin Gonzalez <egonzalezd12@miumg.edu.gt>
2023-03-07 20:56:06 -0600

    Horoscopo

---
+++ Horoscopo/Horoscopo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Horoscopo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola");
+            Console.WriteLine("Ingresa tu fecha de nacimiento");
+            string fechanacimiento = Console.ReadLine();
+
+            DateTime fecha = DateTime.Parse(fechanacimiento);
+            int mes = fecha.Month;
+            int dia = fecha.Day;
+
+            string signoZodiaco = "";
+            if ((mes == 1 && dia >= 1) || (mes == 2 && dia <= 31))
+            {
+                signoZodiaco = "Tauro";
+            }
+            
+            if ((mes == 2 && dia >= 1) || (mes == 3 && dia <= 28))
+            {
+                signoZodiaco = "Aries";
+            }
+            Console.WriteLine("tu signo zodiacal es:" + signoZodiaco);
+            Console.ReadLine();
+        }
+    }
+}
